5d3b1c241afc277dd99db1af29bec480fd12cc01
JoannaKozlowska <joanna.kozlowska2016@gmail.com>
2018-05-10 22:45:17 +0200

    Second Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("Test 1 OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println();
+        Calculator calculator = new Calculator(10, 7);
+        int resultAddition = calculator.addAtoB();
+        int resultSubtraction = calculator.subtractAandB();
+        if (resultAddition > resultSubtraction) {
+            System.out.println("The addition result is: " + resultAddition + ". Test 2 ok.");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println();
+        if (resultSubtraction < resultAddition) {
+            System.out.println("The subtraction result is: " + resultSubtraction + ". Test 3 ok.");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
