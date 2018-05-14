61761a56d057c4201d48a23d42c18e32399ba8a4
JoannaKozlowska <joanna.kozlowska2016@gmail.com>
2018-05-14 20:09:53 +0200

    Test 3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,28 +5,5 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-        String result = simpleUser.getUsername();
-        if (result.equals("theForumUser")){
-            System.out.println("Test 1 OK");
-        } else {
-            System.out.println("Error!");
-        }
-        System.out.println();
-        Calculator calculator = new Calculator(10, 7);
-        int resultAddition = calculator.addAtoB();
-        int resultSubtraction = calculator.subtractAandB();
-        if (resultAddition > resultSubtraction) {
-            System.out.println("The addition result is: " + resultAddition + ". Test 2 ok.");
-        } else {
-            System.out.println("Error!");
-        }
-        System.out.println();
-        if (resultSubtraction < resultAddition) {
-            System.out.println("The subtraction result is: " + resultSubtraction + ". Test 3 ok.");
-        } else {
-            System.out.println("Error!");
-        }
-
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/OddNumbersExterminatorTestSuite.java
@@ -0,0 +1,47 @@
+package com.kodilla.testing.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class OddNumbersExterminatorTestSuite {
+    @Test
+    public void testExterminateEmptyList(){
+        //given
+        ArrayList<Integer> numbers = new ArrayList<>();
+        OddNumbersExterminator oe = new OddNumbersExterminator();
+
+        //when
+        List<Integer> resultList = oe.exterminate(numbers);
+
+        //then
+        Assert.assertEquals(0, resultList.size());
+    }
+    @Test
+    public void testExterminateFullList(){
+        //given
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random theGenerator = new Random();
+        int counter = 0;
+        for (int n = 0; n < 20; n++) {
+            int number = theGenerator.nextInt(20);
+            if(number%2 ==0){
+                counter++;
+            }
+            numbers.add(number);
+        }
+        OddNumbersExterminator oe = new OddNumbersExterminator();
+
+        //when
+        List<Integer> resultList = oe.exterminate(numbers);
+
+        //then
+        Assert.assertEquals(counter, resultList.size());
+        for(int m: resultList) {
+            Assert.assertEquals(0, m%2);
+        }
+    }
+}
