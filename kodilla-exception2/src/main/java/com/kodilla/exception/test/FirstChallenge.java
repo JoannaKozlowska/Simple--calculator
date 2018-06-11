07061972a2a2bc4d60abf02ba3001b1e84d37119
JoannaKozlowska <joanna.kozlowska2016@gmail.com>
2018-06-11 21:38:25 +0200

    Eceptions

--- kodilla-exception2/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception2/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,29 +1,23 @@
 package com.kodilla.exception.test;
 
 public class FirstChallenge {
-        public double divide ( double a, double b) throws ArithmeticException {
-            try {
-                if (b == 0) {
-                    throw new ArithmeticException();
-                }
-                return a / b;
-            }
-            catch (ArithmeticException e){
-                System.out.println("You aren't allowed to divide by zero. Error: " + e);
-            }
-            finally {
-                System.out.println("Arithmetic exception was checked");
-                if (b!=0) {return a / b;}
-                else { return 0;}
-            }
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
+    }
     /**
      * This main can throw an ArithmeticException!!!
      * @param args
      */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("You aren't allowed to divide by zero. Error: " + e);
+        }
     }
 }
